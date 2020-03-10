c51a98a97a11e3bfecd299d136896797498d0d7f
EliseLbz <elise.elbeze@gmail.com>
2020-03-10 11:28:22 +0000

    step 3: add label

--- Clicker/ViewController.swift
+++ Clicker/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    
     @IBAction func buttonTapped(_ sender: Any) {
         print("Hello, world")
     }
